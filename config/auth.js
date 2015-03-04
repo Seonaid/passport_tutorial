d81d30888e8c7191213b523cf6f9670394226b1e
Seonaid <seonaidl@gmail.com>
2015-03-04 12:41:19 -0400

    added twitter authentication

--- config/auth.js
+++ config/auth.js
@@ -1,14 +1,14 @@
 module.exports = {
 	'facebookAuth': {
-		'clientId': '337039586487565',
-		'clientSecret': 'f6bdcca3b778e2e8cb46ecaaea0b7a7a',
-		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+		clientID : 337039586487565,
+		clientSecret: 'f6bdcca3b778e2e8cb46ecaaea0b7a7a',
+		callbackURL : 'http://localhost:8080/auth/facebook/callback'
 	}, 
 
 	'twitterAuth': {
-		'consumerKey' : 'your-consu',
-		'consumerSecret' : '',
-		'callbackURL' : 'http://localhost:8080/auth/twitter/callback'
+		consumerKey : 'T9l4ylNM4mY3j7zQ8jaP70xop',
+		consumerSecret : 'dCVpxH9diz3hgApN1Rhiy2HVDLGhcCY5Vg6OyXdfF6q0Ssh7si',
+		callbackURL : 'http://localhost:8080/auth/twitter/callback'
 	},
 
 	'googleAuth' :{
